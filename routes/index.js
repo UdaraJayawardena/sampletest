e810f1141278e63dd8f519e0e4c239dee45ac7ec
UdaraJayawardena <udarajaywardena5@gmail.com>
2020-01-23 10:30:49 +0530

    ideamart

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,42 @@ router.get('/', function (req, res, next) {
   })
 });
 
+router.post('/userRegistration', (req, res) => {
+  const body = req.body;
+  console.log(body);
 
+  axios.post('https://api.ideamart.io/subscription/send', {
+    applicationId: body.applicationId,
+    password: body.password,
+    version: body.version,
+    action: body.action,
+    subscriberId: body.subscriberId
+  }).then(resss => {
+    res.json(resss.data);
+  })
+})
+
+router.post('/OTPRequest', (req, res) => {
+  const body = req.body;
+  console.log(body);
+
+  axios.post('http://core.sdp:7000/subscription/otp/request', {
+    applicationId: body.applicationId,
+    password: body.password,
+    subscriberId: body.subscriberId,
+    applicationHash: body.applicationHash,
+    applicationMetaData:
+    {
+      client: body.client,
+      device: body.device,
+      os: android8,
+      appCode: ""
+    }
+  }
+  ).then(resss => {
+    res.json(resss.data);
+  })
+})
 
 router.get('/test', (req, res) => {
   res.json('This is a Test Message')
