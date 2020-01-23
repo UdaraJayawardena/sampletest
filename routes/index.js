afdf88af5f43cd800c842aee9cfe8ea14f1ea917
UdaraJayawardena <udarajaywardena5@gmail.com>
2020-01-23 12:17:56 +0530

    otp

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios');
 var http = require('http');
+
 router.get('/', function (req, res, next) {
   axios.get('https://jsonplaceholder.typicode.com/posts').then((ress) => {
     res.json(ress.data);
@@ -29,52 +30,79 @@ router.post('/userRegistration', (req, res) => {
 
 })
 
+// router.post(options, (req, res) => {
+//   const body = req.body;
+//   console.log(body);
+
+// axios.post('http://core.sdp:7000/subscription/otp/request', {
+//   headers: {
+//     "content-type": "application/json",
+//     "accept": "application/json"
+//   }
+// }, {
+//   "applicationId": body.applicationId,
+//   "password": body.password,
+//   "subscriberId": body.subscriberId,
+//   "applicationHash": body.applicationHash,
+//   "applicationMetaData":
+//   {
+//     "client": body.client,
+//     "device": body.device,
+//     "os": body.os,
+//     "appCode": body.appCode
+//   }
+// }
+// ).then(resss => {
+//   res.json(resss.data);
+// })
+
+// })
+
 var options = {
-  host: '52.220.137.97',
+  host: '52.220.137.90',
   port: 3030,
-  path: '/OTPRequest'
+  path: 'http://core.sdp:7000/OTPRequest'
 };
 
-router.post(options, (req, res) => {
-  const body = req.body;
-  console.log(body);
+// http://core.sdp:7000/subscription/otp/request
 
-  axios.post('http://core.sdp:7000/subscription/otp/request', {
-    headers: {
-      "content-type": "application/json",
-      "accept": "application/json"
-    }
-  }, {
-    "applicationId": body.applicationId,
-    "password": body.password,
-    "subscriberId": body.subscriberId,
-    "applicationHash": body.applicationHash,
-    "applicationMetaData":
-    {
-      "client": body.client,
-      "device": body.device,
-      "os": body.os,
-      "appCode": body.appCode
-    }
-  }
-  ).then(resss => {
-    res.json(resss.data);
-  })
+router.get('/OTPRequest', (req, res) => {
+  const body = req.body;
+
+  http.get(options, function (respond) {
+    console.log(respond);
+
+    axios.post(options, {
+      headers: {
+        "content-type": "application/json",
+        "accept": "application/json"
+      }
+    }, {
+      "applicationId": body.applicationId,
+      "password": body.password,
+      "subscriberId": body.subscriberId,
+      "applicationHash": body.applicationHash,
+      "applicationMetaData":
+      {
+        "client": body.client,
+        "device": body.device,
+        "os": body.os,
+        "appCode": body.appCode
+      }
+    }).then(resss => {
+      res.json(resss.data);
+    })
+
+  }).on('error', function (e) {
+    console.log("Got error: " + e.message);
+  });
 })
+
+
 
 router.get('/test', (req, res) => {
   res.json('This is a Test Message')
 })
 
-
-// http.get(options, function (res) {
-//   console.log("Got response: " + res.statusCode);
-
-//   res.on("data", function (chunk) {
-//     console.log("BODY: " + chunk);
-//   });
-// }).on('error', function (e) {
-//   console.log("Got error: " + e.message);
-// });
 
 module.exports = router;
