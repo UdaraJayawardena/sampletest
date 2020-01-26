a3307622bce401ec5c7decc6e2ee5328dcdad712
UdaraJayawardena <udarajaywardena5@gmail.com>
2020-01-26 21:31:16 +0530

    new

--- routes/index.js
+++ routes/index.js
@@ -6,43 +6,32 @@ var querystring = require('querystring');
 var qs = require('qs');
 
 
-
-OTPRequest();
-
-// var options = {
-//   host: 'http://localhost',
-//   port: '3030',
-//   path: 'https://api.dialog.lk/subscription/otp/request',
-//   method: 'POST'
-// };
-
-function OTPRequest() {
+router.post('/OTP', (req, res) => {
 
   var post_data = qs.stringify(
     {
-      "applicationId": "APP_054681",
-      "password": "dd9a5d699c263e82827c378ee08c3f7f",
-      "subscriberId": " tel:94784662138",
-      "applicationMetaData": {
-        "client": "MOBILEAPP",
-        "device": "Samsung S10",
-        "os": "android9",
-        "appCode": "https://play.google.com/store/apps/details?id=lk.dialog.megarunlor"
+      applicationId: "APP_054681",
+      password: "dd9a5d699c263e82827c378ee08c3f7f",
+      subscriberId: "tel:94784662138",
+      applicationMetaData: {
+        client: "MOBILEAPP",
+        device: "Samsung S10",
+        os: "android9",
+        appCode: "https://play.google.com/store/apps/details?id=lk.dialog.megarunlor"
       }
     }
   );
 
-  console.log(post_data)
+  // 'Content-Length': Buffer.byteLength(post_data.length)
 
   var post_options = {
-    host: '52.220.190.37',
-    port: '5050',
-    path: 'https://api.dialog.lk/subscription/otp/request',
+    host: 'api.dialog.lk',
+    port: '443',
+    path: '/subscription/otp/request',
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      "Accept": "application/json",
-      'Content-Length': post_data.length
+      "Accept": "application/json"
     },
     body: post_data
   };
@@ -57,8 +46,9 @@ function OTPRequest() {
   post_req.write(post_data);
   post_req.end();
 
+})
 
-}
+
 
 
 router.get('/', function (req, res, next) {
@@ -66,32 +56,6 @@ router.get('/', function (req, res, next) {
     res.json(ress.data);
   })
 });
-
-// http.get(options, function (res) {
-//   var obj = {
-//     "applicationId": " APP_054681",
-//     "password": "dd9a5d699c263e82827c378ee08c3f7f",
-//     "subscriberId": " tel:94784662138",
-//     "applicationMetaData": {
-//       "client": "MOBILEAPP",
-//       "device": "Samsung S10",
-//       "os": "android8",
-//       "appCode": "https://play.google.com/store/apps/details?id=lk.dialog.megarunlor"
-//     }
-//   }
-
-//   axios.post('https://api.dialog.lk/subscription/otp/request', obj).then(resss => {
-//     res.json(resss.data);
-//   })
-
-// });
-
-// {
-//   headers: {
-//     "Content-Type": "application/json",
-//     "Accept": "application/json"
-//   }
-// },
 
 router.post('/OTPRequest', (req, res) => {
   const body = req.body;
@@ -102,29 +66,24 @@ router.post('/OTPRequest', (req, res) => {
       "Accept": "application/json"
     }
   }, {
-    "applicationId": "APP_054681",
-    "password": "dd9a5d699c263e82827c378ee08c3f7f",
-    "subscriberId": " tel:94784662138",
-    "applicationMetaData": {
-      "client": "MOBILEAPP",
-      "device": "Samsung S10",
-      "os": "android9",
-      "appCode": "https://play.google.com/store/apps/details?id=lk.dialog.megarunlor"
+    applicationId: "APP_054681",
+    password: "dd9a5d699c263e82827c378ee08c3f7f",
+    subscriberId: " tel:94784662138",
+    applicationMetaData: {
+      client: "MOBILEAPP",
+      device: "Samsung S10",
+      os: "android9",
+      appCode: "https://play.google.com/store/apps/details?id=lk.dialog.megarunlor"
     }
   }).then(resss => {
     res.json(resss.data);
   })
 })
 
-router.get('/smsSend', (req, res) => {
+router.post('/smsSend', (req, res) => {
   const body = req.body;
 
-  axios.post('https://api.ideamart.io/sms/send', {
-    headers: {
-      "content-type": "application/json",
-      "accept": "application/json"
-    }
-  }, {
+  axios.post('https://api.dialog.lk/sms/send', {
     "message": body.message,
     "destinationAddresses": body.destinationAddresses,
     "password": body.password,
@@ -137,5 +96,6 @@ router.get('/smsSend', (req, res) => {
 router.get('/test', (req, res) => {
   res.json('This is a Test Message')
 })
+
 
 module.exports = router;
